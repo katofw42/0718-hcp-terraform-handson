f351ad67955e900eefa8b1abf51d242fcfd69688
kato <katofw42@gmail.com>
2025-07-18 17:12:24 +0900

    add tag to ec2

--- main.tf
+++ main.tf
@@ -16,4 +16,8 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
