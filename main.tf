d3f03750c53357497dffde9ec59df921a954c400
kato <katofw42@gmail.com>
2025-07-18 16:59:00 +0900

    add ec2

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
 # main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+
+resource "aws_instance" "web_server" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.micro"
+}
